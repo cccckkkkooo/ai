4f271972a43ffce8d8858091d9d2de65adb0ada2
nnn <120836100+cccckkkkooo@users.noreply.github.com>
2025-04-03 15:44:13 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import g4f
 from flask_cors import CORS
+from threading import Timer
 
 app = Flask(__name__)
 CORS(app)  # Разрешает запросы с других устройств
@@ -60,7 +61,12 @@ def save_preferences():
     except Exception as e:
         print(f"Ошибка сервера: {e}")
         return "Ошибка сервера", 500
+        
+def keep_alive():
+    Timer(60, keep_alive).start()
+    requests.get("https://ai-travel-api.onrender.com/status")
 
+keep_alive()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True, threaded=True, use_reloader=False)
